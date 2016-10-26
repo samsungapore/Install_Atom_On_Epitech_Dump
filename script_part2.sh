9dd69884b30f619077baf24c1df5ecc58b55affd
Kazuhiro <samuel.radat@epitech.eu>
2016-10-26 15:31:08 +0200

    updated scripts

--- script_part2.sh
+++ script_part2.sh
@@ -1,4 +1,5 @@
 nvm install node
 npm install -g npm
 npm config set python /usr/bin/python2 -g
+clear
 echo now launch script_part3.sh
--- script_part3.sh
+++ script_part3.sh
@@ -1,2 +1,5 @@
 gunzip atom-amd64.tar.gz
 tar -xvf atom-amd64.tar
+clear
+cd atom-1.11.2-amd64
+./atom
